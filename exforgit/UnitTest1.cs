500d1297b5d45c25c57d96f2fd3114c24b9a6586
Ruchita <ruchita.mehta87@gmail.com>
2025-10-30 22:38:05 +0100

    new Testcases

---
+++ exforgit/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+
+namespace exforgit
+{
+    [TestFixture]
+
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        [TestCase("https://www.google.com", "Google")]
+        [TestCase("https://www.bing.com", "Bing")]
+        [TestCase("https://www.yahoo.com", "Yahoo")]
+        public void Test1(string url, string expectedTitle)
+        {
+            driver.Navigate().GoToUrl(url);
+            string actualTitle = driver.Title;
+            Assert.IsTrue(actualTitle.Contains(expectedTitle),
+                $"❌ Title check failed! Expected '{expectedTitle}', but got '{actualTitle}'");
+
+
+
+            
+        }
+        public void TearDown()
+        {
+            if (driver != null)
+            {
+                driver.Quit();
+                driver.Dispose();
+                driver = null;
+            }
+
+
+        }
+    }
+}
